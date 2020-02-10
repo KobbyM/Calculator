9666ac21d71cc8880200452306cfbec9c5e06845
Tapiwa <tapiwatiyemba@icloud.com>
2020-02-10 15:03:13 +0000

    class added

---
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package com.qa.calculator;
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		
+		return a+b;
+	}
+	public float multiply(int x,int y)
+	{
+		return x*y;
+	}
+	
+	public int subtract(int num1, int num2) {
+		return num1-num2;
+	}
+	public float modulus(int x, int y) {
+		return x%y;
+	}
+	
+	public double division(double x, double y) {
+		return x/y;
+	}
+
+}
--- src/main/java/com/qa/calculator/Main.java
+++ src/main/java/com/qa/calculator/Main.java
@@ -7,19 +7,5 @@ public class Main {
 	
 	}
 	
-	public int add(int a, int b) {
-		
-		return a+b;
-	}
-	public float multiply(int x,int y)
-	{
-		return x*y;
-	}
-	
-	public int subtract(int num1, int num2) {
-		return num1-num2;
-	}
-	public float modulus(int x, int y) {
-		return x%y;
-	}
+
 }
